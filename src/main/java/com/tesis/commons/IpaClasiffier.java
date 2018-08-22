daecffcd19f314eb33e3334e2759ebb0a4ed1bbd
Franco D'Achilli <francodachilli@gmail.com>
2018-08-22 18:47:55 -0300

    Deteccion de conflictos

--- src/main/java/com/tesis/commons/IpaClasiffier.java
+++ src/main/java/com/tesis/commons/IpaClasiffier.java
@@ -1,6 +1,9 @@
 package com.tesis.commons;
 
 
+import org.ipa.GroupAnalysisResult;
+import org.ipa.GroupAnalysisRow;
+import org.ipa.IpaAnalysis;
 import org.json.JSONException;
 import org.processDataset.DirectProcessing;
 import org.processDataset.PhasesProcessingSingleClassifier;
@@ -8,6 +11,8 @@ import org.processDataset.ProcessDataset;
 import org.weka.*;
 import java.io.FileNotFoundException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class IpaClasiffier {
 
@@ -25,6 +30,27 @@ public class IpaClasiffier {
         Weka weka = new WekaDecisionTable(10, 1, 3);
         PhasesProcessingSingleClassifier process = new PhasesProcessingSingleClassifier(weka, true, true);
         return process.classify(file, "");
+    }
+    
+    
+    public List<String> getConflictos (String path){
+    	//Solo se analiza de a un grupo
+    	List<String> conflictos = new ArrayList<>();
+    	List<String> paths =  new ArrayList<String>() {{
+    			    add(path);}};
+    	IpaAnalysis ipaAnalysis = new IpaAnalysis(paths);
+    	List<GroupAnalysisResult> groupAnalysisResults = ipaAnalysis.analizeGroups();
+    	if (groupAnalysisResults != null && groupAnalysisResults.size()>0) {
+    		for (GroupAnalysisRow result: groupAnalysisResults.get(0).getAnalysisResults()) {
+    			  float porcentaje = result.getPercentage() != null ? Float.parseFloat(result.getPercentage().split("%")[0].replace(",", ".")) : 0;
+    			  float limiteInferior = result.getInfLimit() != null ?  Float.parseFloat(result.getInfLimit().split("%")[0]) : 0;
+    			  float limiteSuperior =  result.getSupLimit() != null ?  Float.parseFloat(result.getSupLimit().split("%")[0]) : 0;
+    			  if (porcentaje < limiteInferior || porcentaje > limiteSuperior) {
+    				  conflictos.add(result.getConflict());
+    			  }
+    		}
+    	}
+    	return null;
     }
 
 
